6fd7f3cb01f621d2ea79e15188f8000c7b6fa361
Dominic May <jack.thatch@gmail.com>
2017-01-26 20:53:18 +0800

    Remove code specific to my system

--- tools/add_feed.py
+++ tools/add_feed.py
@@ -1,39 +1,22 @@
 import os
-from urllib.parse import urlencode, quote
 
 from autobit import Client
 
 
-def add_rarbg_feed(client, name, directory, filter_kwargs):
-    url = 'http://localhost:5555/{}?{}'.format(
-        quote(name),
-        urlencode(filter_kwargs)
-    )
-
-    return client.add_feed(name, url, directory)
-
-
 def main():
     client = Client('http://localhost:8081/gui/', auth=('admin', '20133'))
+    client.get_torrents()
 
     name = input('name> ')
     directory = input('directory> ')
 
     os.makedirs(directory, exist_ok=True)
 
-    if input('rarbg[yn]> ') == 'n':
-        client.add_feed(
-            name,
-            input('url> '),
-            directory
-        )
-    else:
-        add_rarbg_feed(
-            client,
-            name,
-            directory,
-            eval(input('filter dict> '))
-        )
+    client.add_feed(
+        name,
+        input('url> '),
+        directory
+    )
 
 
 if __name__ == '__main__':
